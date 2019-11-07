115ba1d0b6a4dc111aef0b63bca14e446a9ea351
evscruzen <evanscruzen@gmail.com>
2019-11-07 18:15:06 -0500

    Merge branch 'site' of C:\Users\galac\Documents\GitHub\NLP4NM with conflicts.

--- site/__init__.py
+++ site/__init__.py
@@ -18,7 +18,7 @@ def create_app(test_config=None):
         # a default secret that should be overridden by instance config
         SECRET_KEY="dev",
         # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+        DATABASE=os.path.join(app.instance_path, "nlpsite.sqlite"),
     )
 
     if test_config is None:
