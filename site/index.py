b6214fe60ed04661a54229775e5a179eab8a11c6
Wamadahama <smurftown2@gmail.com>
2019-11-07 16:30:17 -0500

    fixed flask bootstrap

--- site/index.py
+++ site/index.py
@@ -1,13 +1,20 @@
-from flask import Blueprint
-from flask import flash
-from flask import g
-from flask import redirect
-from flask import render_template
-from flask import request
-from flask import url_for
-from werkzeug.exceptions import abort
+import sys
+import sqlite3
+import os
+import hashlib
+
+from flask import Flask, request, session, g, redirect, url_for, \
+    abort, render_template, flash, Markup
+
+from contextlib import closing
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.config.from_object(__name__)
 
 @app.route("/")
 def index():
-
     return render_template("input.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
