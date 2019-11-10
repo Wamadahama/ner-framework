f89f83d4d0940195f21aa36c36f1f098bfcf0906
evscruzen <evanscruzen@gmail.com>
2019-11-10 13:57:11 -0500

    Progress

--- site/index.py
+++ site/index.py
@@ -12,13 +12,16 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config.from_object(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/input")
 def input():
     return render_template("input.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=8000)
