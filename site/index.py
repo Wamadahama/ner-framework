ffc62e376d35daa4c96eab016fea3daaa68529a5
evscruzen <evanscruzen@gmail.com>
2019-11-10 16:40:39 -0500

    Merged Evan and Chu's changes to index.html and index.py Chu: Model name now updates in header
    Evan:
    List of models now works

--- site/index.py
+++ site/index.py
@@ -12,15 +12,44 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config.from_object(__name__)
 
+# Set up sqlite database
+
+conn = sqlite3.connect('nlp4nm.db')
+c = conn.cursor()
+
+c.execute('SELECT {cn} FROM {tn}'.\
+        format(tn='model', cn='ModelName'))
+models = c.fetchall()
+
+print(models)
+
+"""
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db #
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+"""
+
+#App routes
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-
-@app.route("/input")
-def input():
-    return render_template("input.html")
+    return render_template("index.html", models=models)
 
 
 if __name__ == '__main__':
