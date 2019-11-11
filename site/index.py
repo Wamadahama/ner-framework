b5f84b888c7b96454ee36ef86b0dd75675dfae69
evscruzen <evanscruzen@gmail.com>
2019-11-10 19:00:35 -0500

    Attempting to send selection to input.html
    Working on sending selection from index.html to input.html, added 3 test files: server.py, student.html, and result.html

--- site/index.py
+++ site/index.py
@@ -21,39 +21,19 @@ c.execute('SELECT {cn} FROM {tn}'.\
         format(tn='model', cn='ModelName'))
 models = c.fetchall()
 
-print(models)
+# App routes
 
-"""
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db #
-
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-"""
-
-#App routes
 
 @app.route("/")
 def index():
     return render_template("index.html", models=models)
 
-@app.route('/input')
+
+@app.route("/input", methods=['POST', 'GET'])
 def input():
-    return render_template("input.html")
+    if request.method == 'POST':
+        result = request.form
+        return render_template("input.html", result=result)
 
 
 if __name__ == '__main__':
