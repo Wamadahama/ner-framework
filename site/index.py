ab3ad55d1231f4bb1aec8dc9ce6f3cbd0c7558a6
evscruzen <evanscruzen@gmail.com>
2019-11-13 12:32:25 -0500

    Deleted tutorial files, reverted db context code

--- site/index.py
+++ site/index.py
@@ -12,6 +12,50 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config.from_object(__name__)
 
+DATABASE = 'nlp4nm.db'
+DEBUG = True
+SECRET_KEY = "shoeball"
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.config.from_object(__name__)
+
+
+# Database setup
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('db/schema/db.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exection):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+@app.errorhandler(404)
+def not_found_error(e):
+    return render_template('./response/404.html')
+
+
+@app.errorhandler(500)
+def bad_code_error(e):
+    return render_template('./response/500.html')
+
 # Set up sqlite database
 
 conn = sqlite3.connect('nlp4nm.db')
