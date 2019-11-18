3f42a7862a81c4279cb70e6f97921b9e6651aaf5
Abdi <amahaseth@umassd.edu>
2019-11-18 01:41:07 -0500

    Demo for the previous commit (in NLP4NM/site/extraction/model/test.py)

---
+++ site/extraction/model/test.py
@@ -0,0 +1,6 @@
+from dataset_management import DataHandler
+
+d_set1 = DataHandler('../../../nlp-model/dataset/MITMovie_dataset.csv')
+
+sentences = d_set1.getSentences()
+X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = d_set1.get_train_test(sentences=sentences, test_size=0.1, max_len=30)
