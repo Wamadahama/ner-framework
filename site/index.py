e31ff1f272ee3504aba5b9034769f579d4bd42b9
Chuefeng <feng.vang98@gmail.com>
2019-11-19 16:24:01 -0500

    model sessions working

--- site/index.py
+++ site/index.py
@@ -4,7 +4,7 @@ import os
 import hashlib
 
 from flask import Flask, request, session, g, redirect, url_for, \
-    abort, render_template, flash, Markup
+    abort, render_template, flash, Markup, session
 
 from contextlib import closing
 
@@ -79,9 +79,14 @@ def input():
     if request.method == 'POST':
         result = request.form.get("model-select")
         selected_model = str(result)
+        session["selected_model"] = selected_model
         return render_template("input.html", selected=selected_model)
     elif request.method == 'GET':
         return render_template("input.html", result=None)
+
+@app.route("/output", methods=['POST', 'GET'])
+def output():
+    return render_template("output.html", result=None)
 
 
 if __name__ == '__main__':
