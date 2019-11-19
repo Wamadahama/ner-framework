962c1ccfad3fe53d410b89d74b71cf28e65b05bf
Wamadahama <smurftown2@gmail.com>
2019-11-19 15:13:45 -0500

    load dataset/model

--- site/extraction/model/dataset.py
+++ site/extraction/model/dataset.py
@@ -3,11 +3,10 @@ from nltk.corpus.reader import *
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
-
+from tensorflow.keras.preprocessing.sequence import pad_sequences 
+from tensorflow.keras.utils import to_categorical
 
 class DataHandler:
-
     def __init__(self, filePath):
         self.filePath = filePath
 
@@ -18,8 +17,9 @@ class DataHandler:
         root = ''
         for i in range( len(folders)-1 ):
             root += folders[i] + '/'
-        self.root = root
-        self.fileName = folders[i+1]
+            self.root = root
+            self.fileName = folders[i+1]
+
 
 
     def getSentences(self):
@@ -36,13 +36,11 @@ class DataHandler:
                     (word, tag) = (row[0], row[1])
                     sent.append( (word, tag) )
             return sentences
-
-
         #works for conll, txt
         else:
             content = ConllCorpusReader(self.root,
                                         self.fileName,
-                                      ['pos', 'words'])
+                                        ['words', 'pos'])
             return content.tagged_sents()
 
 
@@ -72,37 +70,44 @@ class DataHandler:
                 words.append(w)
         unique_words = list(set(words))
         num_words = len(unique_words)
+        self.n_words = num_words
 
         words2index = { w: i for i,w in enumerate(unique_words) }
         X = [[words2index[w] for w in s] for s in X]
 
+        print(sentences)
         tags = []
         for s in sentences:
             for w in s:
                 tags.append(w[1])
         tags = list(set(tags))
         num_tags = len(tags)
+        self.n_tags = num_tags
 
         tags2index = { t: i for i,t in enumerate(tags) }
-        print(tags2index)
-        print()
-        # y = [[tags2index[w[1]] for w in s] for s in sentences]
-
-        # y = pad_sequences(maxlen=max_len, sequences=y, padding="post", value=tags2index["O"])
-
-        y = []
-        padded_sent = []
-        x_index = 0
-        for s in sentences:
-            for i in range(max_len):
-                if i >= len(s):
-                    padded_sent.append( tags2index[ 'O' ] )
-                else:
-                    padded_sent.append( tags2index[ s[i][1] ] )
-
-            y.append(padded_sent)
-            padded_sent = []
-
+        print("")
+        print(tags)
+        print("")
+        y = [[tags2index[w[1]] for w in s] for s in sentences]
+        y = pad_sequences(maxlen=max_len, sequences=y, padding="post", value=tags2index["O"])
+        y = [to_categorical(i, num_classes=num_tags) for i in y]
 
         X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X,y, test_size=test_size)
         return X_TRAIN, X_TEST, Y_TRAIN, Y_TEST
+
+
+    def get_dataset(self):
+        sentences = self.getSentences() 
+        max_len = max([len(sentence) for sentence in sentences])
+        X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = self.get_train_test(sentences=sentences, test_size=0.1, max_len=max_len)
+        return Dataset(X_TRAIN, X_TEST, Y_TRAIN, Y_TEST, max_len, self.n_words, self.n_tags)
+    
+
+class Dataset:
+    def __init__(self, x_train, x_test, y_train, y_test, max_len, n_words, n_tags):
+        self.x_train = x_train
+        self.x_test  = x_test
+        self.y_train = y_train
+        self.max_len = max_len
+        self.n_words = n_words 
+        self.n_tags  = n_tags
--- site/test.py
+++ site/test.py
@@ -1,11 +1,24 @@
 from extraction.model.extractmodel import ExtractionModel
+from extraction.model.dataset import DataHandler 
+from extraction.model.model import Model, BiLstm
+
 
 def main():
-    model = ExtractionModel("movie")
+    # Test Model training:
+
+#from dataset import DataHandler
+    #d_set1 = DataHandler('../../../nlp-model/dataset/MITMovie_dataset.csv')
+    #sentences = d_set1.getSentences()
+    #X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = d_set1.get_train_test(sentences=sentences, test_size=0.1, max_len=30)
+
+    loader  = DataHandler("extraction/datasets/re3d_dataset.txt")
+    dataset = loader.get_dataset()
+    training_model = BiLstm("re3d1", "re3d", dataset, (0,0), 512, 0.1, 0.1, 70, 30, 100).get_model()
+    #model = ExtractionModel("movie")
 #    model.extract("Ryan Gosling in the movie Drive where the driver finds himself entagled in a crime syndicate")
-    dt = model.extract('Blade Runner is a 1982 science fiction film directed by Ridley Scott This film is set in a dystopian future Lost Angeles of 2019') 
-    print(dt)
+    #dt = model.extract('Blade Runner is a 1982 science fiction film directed by Ridley Scott This film is set in a dystopian future Lost Angeles of 2019') 
+    #print(dt)
 #    model.extract("i m thinking of a 1988 action film in which bruce willis a nypd officer fights off terrorists")
-#in which synthetic humans known as replicants are bio-engineered by the powerful Tyrell Corporation to work on off-world Colonies. Written by hampton Francher and David Peoples')
+#in which synthetic ehumans known as replicants e bio-engineered by the powerful Tyrell Corporation to work on off-world Colonies. Written by hampton Francher and David Peoples')
 
 main()
